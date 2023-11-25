356af0231edc7e6b7faeac5e64309a5c71d0f977
Nguyen Hoang Phuong Nam <nhpn2003@gmail.com>
2023-11-26 00:43:24 +0700

    end chapter 9

---
+++ app/dashboard/(overview)/loading.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import DashboardSkeleton from '../../ui/skeletons';
+
+const Loader = () => {
+    return (
+        <DashboardSkeleton />
+    );
+};
+
+export default Loader;
---
+++ app/dashboard/(overview)/page.tsx
@@ -0,0 +1,37 @@
+import { Card } from '../../ui/dashboard/cards'
+import RevenueChart from '../../ui/dashboard/revenue-chart'
+import LatestInvoices from '../../ui/dashboard/latest-invoices'
+import { lusitana } from '../../ui/fonts'
+import { fetchLatestInvoices, fetchCardData, fetchRevenue } from '../../lib/data'
+import { Suspense } from 'react'
+import CardWrapper from '@/app/ui/dashboard/cards';
+import { RevenueChartSkeleton, LatestInvoicesSkeleton, CardsSkeleton } from '@/app/ui/skeletons'
+
+const Page = async () => {
+    const latestInvoices = await fetchLatestInvoices();
+    const revenue = await fetchRevenue();
+    const { numberOfCustomers, numberOfInvoices, totalPaidInvoices, totalPendingInvoices } = await fetchCardData();
+
+    return (
+        <main>
+            <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
+                Dashboard
+            </h1>
+            <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
+                <Suspense fallback={<CardsSkeleton />}>
+                    <CardWrapper />
+                </Suspense>
+            </div>
+            <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
+                <Suspense fallback={<RevenueChartSkeleton />}>
+                    <RevenueChart revenue={revenue} />
+                </Suspense>
+                <Suspense fallback={<LatestInvoicesSkeleton />}>
+                    <LatestInvoices latestInvoices={latestInvoices} />
+                </Suspense>
+            </div>
+        </main>
+    )
+}
+
+export default Page
