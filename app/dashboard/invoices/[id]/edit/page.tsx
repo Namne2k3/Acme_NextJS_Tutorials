e59277ec891977c589036f0ddf11716ff5da9450
Nguyen Hoang Phuong Nam <nhpn2003@gmail.com>
2023-11-30 12:13:13 +0700

    end chapter 16

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -3,7 +3,11 @@ import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
 import { fetchCustomers } from '@/app/lib/data';
 import { fetchInvoiceById } from '@/app/lib/data';
 import { notFound } from 'next/navigation';
+import { Metadata } from 'next'
 
+export const metadata: Metadata = {
+    title: 'Edit Invoice'
+}
 const Page = async ({ params }: { params: { id: string } }) => {
     const id = params.id;
 
--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -10,7 +10,8 @@ import { ArrowRightIcon } from '@heroicons/react/20/solid';
 import { Button } from './button';
 import { useFormState, useFormStatus } from 'react-dom';
 import { authenticate } from '../lib/action';
-import { useSession } from 'next-auth/react';
+import Image from 'next/image';
+import { signIn } from 'next-auth/react';
 export default function LoginForm() {
 
   const [errorMessage, dispatch] = useFormState(authenticate, undefined);
@@ -62,6 +63,7 @@ export default function LoginForm() {
           </div>
         </div>
         <LoginButton />
+        <LoginGoogleButton />
         <div
           className="flex h-8 items-end space-x-1"
           aria-live="polite"
@@ -79,6 +81,19 @@ export default function LoginForm() {
         </div>
       </div>
     </form>
+  );
+}
+
+const LoginGoogleButton = () => {
+  return (
+
+    <button
+      onClick={() => signIn('google')}
+      className="px-4 w-full mt-4 py-2 border flex gap-2 border-slate-200 dark:border-slate-700 rounded-lg text-slate-700 hover:border-slate-400 dark:hover:border-slate-500 hover:text-slate-900 dark:hover:text-slate-300 hover:shadow transition duration-150">
+      <Image width={24} height={24} src="https://www.svgrepo.com/show/475656/google-color.svg" loading="lazy" alt="google logo" />
+      <span>Login with Google</span>
+    </button>
+
   );
 }
 
